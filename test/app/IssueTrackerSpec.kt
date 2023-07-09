38c37571a0d38020ae826f3b353f77002ca2a3f1
sg <sgulyaev@gmail.com>
2023-07-09 13:21:26 +0300

    refactoring, introduce some helper extensions to calculate time in minutes. Maybe later we add some class for turnaroundTime (maybe not if it is already readable enough)

--- test/app/IssueTrackerSpec.kt
+++ test/app/IssueTrackerSpec.kt
@@ -14,7 +14,7 @@ class IssueTrackerSpec {
     fun `should just add turnaround time in minutes to the start date if no overflows`() {
         test(
             startDate = monday,
-            turnaroundTime = 1,
+            turnaroundTime = 1.min,
             expectedDueDate = monday.copy(minute = 1)
         )
     }
@@ -24,19 +24,19 @@ class IssueTrackerSpec {
     fun `should add hours if there is overflow in minutes`() {
         test(
             startDate = monday.copy(minute = 59),
-            turnaroundTime = 1,
+            turnaroundTime = 1.min,
             expectedDueDate = monday.copy(hour = 10, minute = 0)
         )
 
         test(
             startDate = monday,
-            turnaroundTime = 60,
+            turnaroundTime = 1.hour,
             expectedDueDate = monday.copy(hour = 10, minute = 0)
         )
 
         test(
             startDate = monday.copy(minute = 55),
-            turnaroundTime = 67,
+            turnaroundTime = 1.hour + 7.min,
             expectedDueDate = monday.copy(hour = 11, minute = 2)
         )
     }
@@ -45,19 +45,19 @@ class IssueTrackerSpec {
     fun `should add day if overflow in hours, working day starts at 9-00 and ends at 17-00`() {
         test(
             startDate = monday.copy(hour = 16, minute = 59),
-            turnaroundTime = 1,
+            turnaroundTime = 1.min,
             expectedDueDate = tuesday.copy(hour = 9, minute = 0)
         )
 
         test(
             startDate = monday.copy(hour = 9, minute = 7),
-            turnaroundTime = 9 * 60 + 5,
+            turnaroundTime = 1.workingDay + 1.hour + 5.min,
             expectedDueDate = tuesday.copy(hour = 10, minute = 12)
         )
 
         test(
             startDate = monday.copy(hour = 9, minute = 7),
-            turnaroundTime = 8 * 60 * 2,
+            turnaroundTime = 2.workingDay,
             expectedDueDate = wednesday.copy(hour = 9, minute = 7)
         )
     }
@@ -70,4 +70,7 @@ class IssueTrackerSpec {
     }
 
 
+    private val Int.workingDay: Int get() = this * 60 * 8
+    private val Int.hour: Int get() = this * 60
+    private val Int.min: Int get() = this
 }
