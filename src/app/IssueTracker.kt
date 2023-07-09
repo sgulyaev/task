fe8b5bd45feeea4457c7aff32bed9280c1ac0c94
sg <sgulyaev@gmail.com>
2023-07-09 13:13:56 +0300

    cases with addition of hours

--- src/app/IssueTracker.kt
+++ src/app/IssueTracker.kt
@@ -1,11 +1,20 @@
 package app
 
 class IssueTracker {
+    private val MINUTES_PER_HOUR = 60
+
+    private val DAY_START_HOUR = 9
+    private val DAY_LENGTH = 8
+
     fun calculateDueDate(date: Date, turnaroundTime: Int): Date {
-        val minutes = date.minute + turnaroundTime
+        val minutesFromDayStart = date.minute + turnaroundTime
+        val workingHoursFromDayStart = (date.hour - DAY_START_HOUR) + minutesFromDayStart / MINUTES_PER_HOUR
+        val extraDays = workingHoursFromDayStart / DAY_LENGTH
+
         return date.copy(
-            minute = minutes % 60,
-            hour = date.hour + minutes / 60
+            minute = minutesFromDayStart % MINUTES_PER_HOUR,
+            hour = DAY_START_HOUR + workingHoursFromDayStart % DAY_LENGTH,
+            day = date.day + extraDays
         )
     }
 }
--- test/app/IssueTrackerSpec.kt
+++ test/app/IssueTrackerSpec.kt
@@ -4,6 +4,8 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 private val monday = Date(year = 2023, month = 7, day = 10, hour = 9, minute = 0)
+private val tuesday = Date(year = 2023, month = 7, day = 11, hour = 9, minute = 0)
+private val wednesday = Date(year = 2023, month = 7, day = 12, hour = 9, minute = 0)
 
 class IssueTrackerSpec {
     val tracker = IssueTracker()
@@ -36,6 +38,27 @@ class IssueTrackerSpec {
             startDate = monday.copy(minute = 55),
             turnaroundTime = 67,
             expectedDueDate = monday.copy(hour = 11, minute = 2)
+        )
+    }
+
+    @Test
+    fun `should add day if overflow in hours, working day starts at 9-00 and ends at 17-00`() {
+        test(
+            startDate = monday.copy(hour = 16, minute = 59),
+            turnaroundTime = 1,
+            expectedDueDate = tuesday.copy(hour = 9, minute = 0)
+        )
+
+        test(
+            startDate = monday.copy(hour = 9, minute = 7),
+            turnaroundTime = 9 * 60 + 5,
+            expectedDueDate = tuesday.copy(hour = 10, minute = 12)
+        )
+
+        test(
+            startDate = monday.copy(hour = 9, minute = 7),
+            turnaroundTime = 8 * 60 * 2,
+            expectedDueDate = wednesday.copy(hour = 9, minute = 7)
         )
     }
 
